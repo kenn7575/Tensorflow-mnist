f34a54129fac12fa4e985089e84a21763722a748
Kemmi Kollemorten <kenn7575@gmail.com>
2022-11-23 10:36:50 +0100

    code cleanup

--- 3. project ,py/model_creation.py
+++ 3. project ,py/model_creation.py
@@ -12,8 +12,6 @@ data = keras.datasets.mnist  # get data
 train_images = train_images/255.0
 train_images = train_images/255.0
 
-# output categorie classes
-class_names = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 model = keras.Sequential([
     # input layer 784/28*28 neurons
